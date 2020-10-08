a9318f53adaf2251599a91632663bc3500c517a7
kth321 <rnxogud136@gmail.com>
2020-10-08 13:17:32 +0900

    while grammer

---
+++ Baekjoon Soulution/01100/1110.py
@@ -0,0 +1,14 @@
+cnt = 0
+n = int(input())
+num = n
+
+while True:
+    last = n%10
+    new = n//10 + last
+    n = last*10 + new%10
+    cnt += 1
+    print(n)
+    if n == num:
+        break
+
+print(cnt)
