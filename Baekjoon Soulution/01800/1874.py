539fdecda1fce8e11c2ab86bfdde6ccbd60f72d2
kth321 <rnxogud136@gmail.com>
2020-10-15 02:14:39 +0900

    change string to list

--- Baekjoon Soulution/01800/1874.py
+++ Baekjoon Soulution/01800/1874.py
@@ -1,20 +1,19 @@
 T = int(input())
 stack = []
-result = ""
+result = []
 last = 1
-flag = False
+flag = True
 for _ in range(T):
     n = int(input())
-    if len(stack) >= 1:
-        if n < stack[-1]:
-            flag = True            
+    if len(stack) >= 1 and n < stack[-1]:
+            flag = False 
     while last <= n:
         stack.append(last)
-        result += '+'
+        result.append('+')
         last += 1
     stack.pop()
-    result += '-'
-if flag is False:
+    result.append('-')
+if flag is True:
     for s in result:
         print(s)
 else:
