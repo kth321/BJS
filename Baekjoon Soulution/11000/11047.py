31879fba206a9e40487589a8d0b2bab43bef58a5
kth321 <rnxogud136@gmail.com>
2020-10-15 02:15:00 +0900

    value name changed

--- Baekjoon Soulution/11000/11047.py
+++ Baekjoon Soulution/11000/11047.py
@@ -2,10 +2,10 @@ coins = []
 N, K = map(int, input().split())
 for _ in range(N):
     coins.append(int(input()))
-ans = 0
+cnt = 0
 coins.reverse()
 for coin in coins:
     if K // coin > 0:
         tmp, K = divmod(K, coin)
-        ans += tmp
-print(ans)
+        cnt += tmp
+print(cnt)        
