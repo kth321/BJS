9e4da7da02332500b40ea1f41cdf0676566ee03c
kth321 <rnxogud136@gmail.com>
2020-10-15 05:03:12 +0900

    modifying

--- Baekjoon Soulution/02800/2839.py
+++ Baekjoon Soulution/02800/2839.py
@@ -1,19 +1,7 @@
 N = int(input())
-available_5kg = N//5
-result = -1
-for i in range(available_5kg, -1, -1):
-    bag_5kg = i
-    remain = N - (5 * i)
-    if remain == 0:
-        result = bag_5kg
+for i in range(N // 5, -1, -1):
+    if ((N - i * 5) % 3 == 0):
+        print(i + ((N - i * 5) // 3))
         break
-    if remain // 3 != 0:
-        bag_3kg = remain // 3
-        remain %= 3
-        if remain != 0:
-            continue
-        else:
-            result = bag_5kg + bag_3kg
-            break
-print(result)
-            
+    elif i == 0 and N % 3 != 0:
+        print(-1)
