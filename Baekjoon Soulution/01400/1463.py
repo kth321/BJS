a506d490cf7c3f8b8257e4196a3524dc46a835c7
kth.mac <rnxogud136@gmail.com>
2020-10-17 15:10:53 +0900

    1463: dynamic programming

---
+++ Baekjoon Soulution/01400/1463.py
@@ -0,0 +1,21 @@
+num = [0, 0]
+
+x = int(input())
+num = num + [None] * (x - 1)
+for i in range(2, x + 1):
+    tmp = i
+    cnt = 0
+    while i != 1:
+        if i % 3 == 0:
+            i = i // 3
+        else:
+            if i % 2 == 0:
+                if num[i//2] > num[i-1]:
+                    i -= 1
+                else:
+                    i = i // 2
+            else:
+                i -= 1 
+        cnt += 1
+    num[tmp] = cnt
+print(num[-1])
