704cdb2fae1ba44b1651b23f1491755a7c3ea8a7
kth.mac <rnxogud136@gmail.com>
2020-11-12 17:01:00 +0900

    2953: math

--- Baekjoon Soulution/02900/2953.py
+++ Baekjoon Soulution/02900/2953.py
@@ -3,4 +3,4 @@ score = [0 for _ in range(5)]
 for i in range(5):
     score[i] = sum(list(map(int, input().split())))
 max = max(score)
-print(score.index(max), max)
+print(score.index(max) + 1, max)
